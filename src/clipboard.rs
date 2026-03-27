00444c1039fc3ab0ed265bf055e84a324baf1361
lbulla <118393328+lbulla@users.noreply.github.com>
2026-03-27 10:34:16 +0100

    Clipboard handling

--- src/clipboard.rs
+++ src/clipboard.rs
@@ -1,10 +1,44 @@
-#[cfg(target_os = "macos")]
-use crate::macos as platform;
-#[cfg(target_os = "windows")]
-use crate::win as platform;
-#[cfg(target_os = "linux")]
-use crate::x11 as platform;
+use arboard::Clipboard;
+#[cfg(all(
+    unix,
+    not(any(target_os = "macos", target_os = "android", target_os = "emscripten"))
+))]
+use arboard::{GetExtLinux, LinuxClipboardKind, SetExtLinux};
 
-pub fn copy_to_clipboard(data: &str) {
-    platform::copy_to_clipboard(data)
+pub use arboard::Error;
+
+pub fn copy_to_clipboard(data: &str, #[allow(unused)] primary: bool) -> Result<(), Error> {
+    Clipboard::new().and_then(|mut clipboard| {
+        let set = clipboard.set();
+
+        #[cfg(all(
+            unix,
+            not(any(target_os = "macos", target_os = "android", target_os = "emscripten"))
+        ))]
+        let set = set.clipboard(if primary {
+            LinuxClipboardKind::Primary
+        } else {
+            LinuxClipboardKind::Clipboard
+        });
+
+        set.text(data)
+    })
+}
+
+pub fn paste_from_clipboard(#[allow(unused)] primary: bool) -> Result<String, Error> {
+    Clipboard::new().and_then(|mut clipboard| {
+        let get = clipboard.get();
+
+        #[cfg(all(
+            unix,
+            not(any(target_os = "macos", target_os = "android", target_os = "emscripten"))
+        ))]
+        let get = get.clipboard(if primary {
+            LinuxClipboardKind::Primary
+        } else {
+            LinuxClipboardKind::Clipboard
+        });
+
+        get.text()
+    })
 }
